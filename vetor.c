82755771f6ed283e94bc144bc8ab7c86b7923fde
enricopereira1 <enrico.pereira@audiogene.com.br>
2025-08-14 11:00:34 -0300

    vetoresssss

--- vetor.c
+++ vetor.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main() {
-  printf("Testeaaaaa");
-  return 0;
+  
+    int n;
+    printf("\nQuantidade de dados a serem lidos:");
+    scanf("%d", &n);
+    
+    int idades[n];
+
+    float soma = 0;
+    for (int i = 0; i < n; i++){
+        printf("\nInforme a %d ª idade: ", i+1);
+        scanf("%d", idades[i]);
+        soma += idades[i]; 
+    }
+    float media = soma/n;
+
+    int a;
+    while (a < n){
+        printf("\nIdade na posição %d:", idades[a]);
+        a++;
+    }
+    
+    printf("\nMédia das idades: %f", media);
+
+    return 0;
 }
